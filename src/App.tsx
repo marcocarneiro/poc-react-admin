cfbf3de0327cf060d319ad2e83612f8981c1dbf5
marcocarneiro <marco.carneiro7@gmail.com>
2024-04-02 19:58:45 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,13 @@ import { UserList } from "./components/UseList"
 import PostIcon from "@mui/icons-material/Book"
 import UserIcon from "@mui/icons-material/Group"
 import { Dashboard } from "./components/Dashboard"
-import ptBrMessages from 'ra-language-pt-br';
+import ptBrMessages from 'ra-language-pt-br'
 import polyglotI18nProvider from 'ra-i18n-polyglot'
 
 const messages = {
   'pt-br': ptBrMessages,
-};
-//const i18nProvider = polyglotI18nProvider(locale => messages[locale], 'pt-br')
-const i18nProvider = polyglotI18nProvider((locale: string) => messages[locale], 'pt-br');
+}
+const i18nProvider = polyglotI18nProvider((locale: string) => messages[locale], 'pt-br')
 
 
 export const App = () => (
