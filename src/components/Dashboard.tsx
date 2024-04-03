efa1345a746595476dc44fea57b44f857ba16fe0
marcocarneiro <marco.carneiro7@gmail.com>
2024-04-03 11:46:23 -0300

    Testes em layout e deploy

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,8 +1,44 @@
+import Stack from '@mui/material/Stack'
 import { Card, CardContent, CardHeader } from "@mui/material"
+import { Link } from "@mui/material"
+import Typography from '@mui/material/Typography'
+import PostIcon from "@mui/icons-material/Book"
+import UserIcon from "@mui/icons-material/Group"
 
 export const Dashboard = () => (
-    <Card>
-        <CardHeader title="Welcome to the administration" />
-        <CardContent>Lorem ipsum sic dolor amet...</CardContent>
-    </Card>
+    <Stack
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+        sx={{ height: '100%' }}
+    >
+        <Stack>
+            <Link href="#/users" sx={{ textDecoration: "none" }} >
+                <Card>
+                    <CardHeader title="Usuários" />
+                    <CardContent>
+                        <UserIcon />
+                        <Typography variant="p" component="div">
+                            Lista com todos os usuários
+                        </Typography>          
+                    </CardContent>
+                </Card>
+            </Link>
+        </Stack>
+
+        <Stack>
+            <Link href="#/posts" sx={{ textDecoration: "none" }} >
+                <Card>
+                    <CardHeader title="Posts" />
+                    <CardContent>
+                        <PostIcon />
+                        <Typography variant="p" component="div">
+                            Posts dos usários
+                        </Typography>          
+                    </CardContent>
+                </Card>
+            </Link>
+        </Stack>                
+    </Stack>
 )
---
+++ src/components/helpers/Estilos.ts
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled'
+
+
+export const ContainerH100 = styled.div`
+    .RaLayout-content, .css-8ajv8q {
+        height: 100vh !important;
+    }
+`
